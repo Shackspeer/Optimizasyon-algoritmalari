bcda87af2299cdc63852f923d90ae32eb6e013d9
Libertas <emreukbeornek@outlook.com>
2025-11-23 17:20:13 +0300

    File update --> DiferansiyelGelişimAlgoritmasi.py

--- Diferansiyel Gelişim Algoritması/DiferansiyelGelişimAlgoritmasi.py
+++ Diferansiyel Gelişim Algoritması/DiferansiyelGelişimAlgoritmasi.py
@@ -1,106 +1,102 @@
 import numpy as np
 
+# Parametreler
+NP = 20          # Popülasyon sayısı
+D = 2            # Değişken sayısı
+F = 0.6          # Mutasyon ölçekleme faktörü
+CR = 0.65        # Çaprazlama oranı
+Gmax = 15         # Maksimum iterasyon
+Lower = -5       # Alt sınır
+Upper = 5        # Üst sınır
 
 
-NP = 20  # Popülasyon büyüklüğü
-D = 2 # Vektör boyutu
-F = 0.5 # Ölçekleme Faktörü
-CR  = 0.6 # Çaprazlama olasılığı
-Gmax = 10 # Maximum iterasyon sayısı
-Lower = -5
-Upper = 5
-M_population = []
+# ------------------------------------------------------
+#  Amaç ve Fitness Fonksiyonları
+# ------------------------------------------------------
+def CostFunction(vec):
+    x, y = vec
+    return (x - 3)**2 + (y - 1)**2
+
+def FitnessFunction(vec):
+    cost = CostFunction(vec)
+    return 1 / (1 + cost)
 
 
-
-def FitnessFunction(x,y):
-    
-    f = (x**2 - 9) + (y**3 - 8)
-        
-    return f
-
-
-    
+# ------------------------------------------------------
+#  Başlangıç Popülasyonu
+# ------------------------------------------------------
+def CreatePopulation(NP, D, Lower, Upper):
+    return np.random.uniform(Lower, Upper, (NP, D))
 
 
-def CreatePopulation(): # D boyutunda NP adet populasyon oluştur
-    population = []
-    for i in range(1,NP+1):
-        i = np.random.uniform(Lower,Upper,4)
-        population.append(i)
-    return population
-
-
-
-def Mutation(population):
-    tempArray = []
+# ------------------------------------------------------
+#  Mutasyon
+# ------------------------------------------------------
+def Mutation(population, F):
     NP = len(population)
-    for x in range(NP):
-        condidates = list(range(NP))
-        condidates.remove(x)
-        r1,r2,r3 = np.random.choice(condidates,3,replace=False)
-        v = population[r1] + F*(population[r2]-population[r3])
-        tempArray.append(v)
-    return np.array(tempArray)
+    mutants = np.zeros_like(population)
 
-
-def Crossover(population,M_population,CR,NP,D):
-    trial_population = np.zeros((NP,D))
     for i in range(NP):
-        randValues = np.random.rand(D)
-        print(randValues)
-        j_Rand = np.random.randint(0,D)
+        idxs = [idx for idx in range(NP) if idx != i]
+        r1, r2, r3 = np.random.choice(idxs, 3, replace=False)
+
+        mutants[i] = population[r1] + F * (population[r2] - population[r3])
+
+    return mutants
+
+
+# ------------------------------------------------------
+#  Çaprazlama
+# ------------------------------------------------------
+def Crossover(population, mutants, CR):
+    NP, D = population.shape
+    trial = np.zeros_like(population)
+
+    for i in range(NP):
+        j_rand = np.random.randint(0, D)
         for j in range(D):
-            if randValues[j] <= CR or j == j_Rand:
-                trial_population[i][j] = M_population[i][j]
+            if np.random.rand() <= CR or j == j_rand:
+                trial[i, j] = mutants[i, j]
             else:
-                trial_population[i][j] = population[i][j]
-    return trial_population
+                trial[i, j] = population[i, j]
+
+    return trial
 
 
-def Selection(population,trial_population,NP,D):
-
-    new_population = np.zeros((NP,D))
+# ------------------------------------------------------
+#  Seleksiyon
+# ------------------------------------------------------
+def Selection(population, trial):
+    NP = len(population)
+    new_population = np.zeros_like(population)
 
     for i in range(NP):
-        old_fitness = FitnessFunction(population[i][0],population[i][1])
-        trial_fitness = FitnessFunction(trial_population[i][0],trial_population[i][0])
-
-        if old_fitness > trial_fitness:
-            new_population[i] = trial_population[i]
+        if FitnessFunction(trial[i]) > FitnessFunction(population[i]):
+            new_population[i] = trial[i]
         else:
             new_population[i] = population[i]
-    
+
     return new_population
 
-population = CreatePopulation()
-M_population = Mutation(population)
-trial_population = Crossover(population,M_population,CR,NP,D)
+
+# ------------------------------------------------------
+#  Ana Döngü
+# ------------------------------------------------------
+def DifferentialEvolution(NP, D, Gmax, F, CR, Lower, Upper):
+
+    population = CreatePopulation(NP, D, Lower, Upper)
+
+    for gen in range(Gmax):
+        mutants = Mutation(population, F)
+        trial = Crossover(population, mutants, CR)
+        population = Selection(population, trial)
+
+    print("\nFinal Popülasyon:")
+    for vec in population:
+        print(vec, " fitness=", round(FitnessFunction(vec), 6))
 
 
-
-
-'''
-for x in population:
-    print(x)
-print("\nMutation values:\n\n")
-for m in M_population:
-    print(m)
-print("\nTrial values:\n\n")
-for t in trial_population:
-    print(t)
-print("\n New population :\n\n")
-
-'''
-for x in population:
-    print(x)
-print("\nMutation values:\n\n")
-for m in M_population:
-    print(m)
-print("\nTrial values:\n\n")
-for t in trial_population:
-    print(t)
-print("\n New population :\n\n")
-for s in Selection(population,trial_population,NP,D):
-    print(s)
-
+# ------------------------------------------------------
+# Çalıştır
+# ------------------------------------------------------
+DifferentialEvolution(NP, D, Gmax, F, CR, Lower, Upper)
